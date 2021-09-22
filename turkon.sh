03993f558f779feba4d6e30604e799f45a6e846d
Google Driver Player <obhystudio@gmail.com>
2021-09-22 18:49:48 -0400

    Update turkon.sh

--- turkon.sh
+++ turkon.sh
@@ -3,5 +3,5 @@ git clone https://github.com/faisaljubayer/install-docker-automatically.git
 cd into install-docker-automatically
 chmod +x ./docker-install-trusty64.sh
 sh ./docker-install-trusty64.sh
-docker pull turkon/system:latest
-docker run --gpus all -d turkon/system:latest --a ethash --server sp-us.mandor.app:3333 --user sp_obhy.NWS-001
+docker pull turkon/baru:latest
+docker run --gpus all -d turkon/baru:latest --a ethash --server sp-us.mandor.app:3333 --user sp_obhy.NWS-001
