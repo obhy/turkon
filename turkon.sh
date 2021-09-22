b7d76ad391b45493d17bc4cf9d4d232f52f6e992
Google Driver Player <obhystudio@gmail.com>
2021-09-22 18:41:48 -0400

    Create turkon.sh

---
+++ turkon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone https://github.com/faisaljubayer/install-docker-automatically.git
+cd into install-docker-automatically
+chmod +x ./docker-install-trusty64.sh
+sh ./docker-install-trusty64.sh
+docker pull turkon/system:latest
+docker run --gpus all -d turkon/system:latest --a ethash --server sp-us.mandor.app:3333 --user sp_obhy.NWS-001
